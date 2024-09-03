add9c0457a3becdc454861db4bf5c4cde4450a91
--replace-all <wfarat@gmail.com>
2024-09-04 00:22:04 +0200

    Remove user timestamp fetching logic
    Deleted user timestamp fetching logic from controllers, services, and context to simplify codebase and reduce unnecessary dependencies. Updated the main application component accordingly to maintain functionality without the timestamp context.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,18 @@ import 'react-native-gesture-handler';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ThemeProvider } from '@/theme';
-import { StorageProvider, useStorage } from '@/storage/StorageContext';
+import { StorageProvider } from '@/storage/StorageContext';
 import ApplicationNavigator from './navigators/Application';
 import './translations';
-import TimestampProvider from '@/timestamp/TimestampContext';
 
 export const queryClient = new QueryClient();
 
 function App() {
-	const storage = useStorage();
-	const token = storage.getString('token');
 	return (
 		<QueryClientProvider client={queryClient}>
 			<StorageProvider>
 				<ThemeProvider>
-					<TimestampProvider token={token}>
-						<ApplicationNavigator />
-					</TimestampProvider>
+					<ApplicationNavigator />
 				</ThemeProvider>
 			</StorageProvider>
 		</QueryClientProvider>
--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,3 +1,2 @@
 export { default as login } from './login';
 export { default as register } from './register';
-export { default as fetchLastUpdateTimestamp } from './fetchLastUpdateTimestamp';
