ec86fb9da8c970b8c9f3c0b9fc19d4ed4f20962b
--replace-all <wfarat@gmail.com>
2024-09-09 19:30:27 +0200

    Remove Flipper integration
    Deleted ReactNativeFlipper.kt and references to Flipper in codebase, including dependencies in build.gradle and package.json. This simplifies the project setup and reduces debug dependencies.

--- android/app/src/main/java/com/plannerapp/MainApplication.kt
+++ android/app/src/main/java/com/plannerapp/MainApplication.kt
@@ -1,12 +1,10 @@
+
 package com.plannerapp
 
 import android.app.Application
-import android.content.Context
-import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
@@ -37,32 +35,10 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        Log.d("MainApplication", "onCreate called")
         SoLoader.init(this, false)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
-        }
-        Log.d("MainApplication", "Before initializing Flipper")
-        initializeFlipper(this, reactNativeHost.reactInstanceManager)
-        Log.d("MainApplication", "After initializing Flipper")
-    }
-
-    private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-                Log.d("MainApplication", "Attempting to initialize Flipper")
-                val className = "com.plannerapp.ReactNativeFlipper"
-                val aClass = Class.forName(className)
-                val method = aClass.getMethod("initializeFlipper", Context::class.java, ReactInstanceManager::class.java)
-                method.invoke(null, context, reactInstanceManager)
-                Log.d("MainApplication", "Flipper initialized successfully")
-            } catch (e: Exception) {
-                Log.e("MainApplication", "Error initializing Flipper: ${e.message}")
-                e.printStackTrace()
-            }
-        } else {
-            Log.d("MainApplication", "Not in DEBUG mode, skipping Flipper initialization")
         }
     }
 }
