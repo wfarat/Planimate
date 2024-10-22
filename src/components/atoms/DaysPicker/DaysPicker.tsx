be714a70fc7bb8b573c0a729d568070e34a6a02b
--replace-all <wfarat@gmail.com>
2024-10-22 22:24:04 +0200

    Add localized day abbreviations to DaysPicker component
    Introduced localized day abbreviations in `common.json` files for both English and Polish. Updated `DaysPicker.tsx` to use these translations and enhanced styling with theme layout integration.

--- src/components/atoms/DaysPicker/DaysPicker.tsx
+++ src/components/atoms/DaysPicker/DaysPicker.tsx
@@ -1,19 +1,39 @@
 import { Text, View } from 'react-native';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import { useTheme } from '@/theme';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
 	pickedDays: boolean[];
 	setPickedDays: (days: boolean[]) => void;
 };
 function DaysPicker({ pickedDays, setPickedDays }: Props) {
+	const { layout, fonts } = useTheme();
+	const { t } = useTranslation(['common']);
 	const handleCheckboxPress = (index: number, checked: boolean) => {
 		const updatedDays = [...pickedDays];
 		updatedDays[index] = checked;
 		setPickedDays(updatedDays);
 	};
 	return (
-		<View>
+		<View
+			style={[
+				layout.row,
+				layout.wrap,
+				layout.justifyCenter,
+				layout.itemsCenter,
+			]}
+		>
 			{pickedDays.map((day, index) => {
+				const days = ['mon, tue, wed, thu, fri, sat, sun'];
+				const dayString = days[index] as
+					| 'mon'
+					| 'tue'
+					| 'wed'
+					| 'thu'
+					| 'fri'
+					| 'sat'
+					| 'sun';
 				return (
 					<View key={index}>
 						<BouncyCheckbox
@@ -21,7 +41,9 @@ function DaysPicker({ pickedDays, setPickedDays }: Props) {
 								handleCheckboxPress(index, checked)
 							}
 						/>
-						<Text>Something</Text>
+						<Text style={fonts.gray400}>
+							{t(`common:daysShort.${dayString}`)}
+						</Text>
 					</View>
 				);
 			})}
