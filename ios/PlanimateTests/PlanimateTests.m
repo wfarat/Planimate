ed0ba42bd7fa7921edebc1b8705e734f51b1b2ca
--replace-all <wfarat@gmail.com>
2025-05-25 17:38:00 +0200

    Rename project from PlannerApp to Planimate
    All references to "PlannerApp" have been updated to "Planimate," including file paths, asset names, and project configuration files. This ensures consistent project naming across the codebase. No functional changes have been made.

--- ios/PlanimateTests/PlanimateTests.m
+++ ios/PlanimateTests/PlanimateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PlannerAppTests : XCTestCase
+@interface PlanimateTests : XCTestCase
 
 @end
 
-@implementation PlannerAppTests
+@implementation PlanimateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
